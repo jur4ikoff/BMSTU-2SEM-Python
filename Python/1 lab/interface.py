f41f3ad2d04c37b33f855a8161973a4e57c56706
1234 <1234>
2024-02-09 22:51:48 +0300

    itog

--- Python/1 lab/interface.py
+++ Python/1 lab/interface.py
@@ -1,6 +1,7 @@
 import sys
 import random
-
+import warnings
+from Convert import test_input_data, convert10to3, convert3to10
 from PyQt6.QtGui import QPolygon
 from sys import argv, exit
 from PyQt6 import uic
@@ -12,93 +13,122 @@ from PyQt6.QtWidgets import QWidget, QPushButton
 from PyQt6 import uic
 
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
 from PyQt6.QtWidgets import QLCDNumber, QLabel, QMainWindow
+from PyQt6.QtGui import QAction
 from PyQt6 import QtCore
 
-
-# if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
-#    QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
-# if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
-#    QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+warnings.filterwarnings('ignore')
 
 
-class Calc(QWidget):
+
+class Calculator_Interface(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
-        # self.setFixedSize(1280, 900)
-        # self.pushButton = QPushButton(self)
-        # self.pushButton.resize(100, 50)
-        # self.pushButton.move(500, 800)
-        # self.pushButton.setText('новый круг')
-        # self.pushButton.clicked.connect(self.paint)
-
-    def initUI(self):
-        # Зададим размер и положение нашего виджета,
-        self.setGeometry(300, 300, 600, 900)
-        # self.setFixedSize(600, 900)
-        # А также его заголовок
-        self.setWindowTitle('Калькулятор систем счисления')
-        self.btn = QPushButton('Кнопка', self)
-        # Подстроим размер кнопки под надпись на ней
-        self.btn.resize(self.btn.sizeHint())
-        self.btn.move(100, 150)
-        # Обратите внимание: функцию не надо вызывать :)
-        self.btn.clicked.connect(self.inc_click)
-
-        self.label = QLabel(self)
-        # Текст задается также, как и для кнопки
-        self.label.setText("Количество нажатий на кнопку")
-        self.label.move(80, 30)
-
-        self.LCD_count = QLCDNumber(self)
-        self.LCD_count.move(110, 80)
-
-        self.count = 0
-
-    def inc_click(self):
-        self.count += 1
-        # В QLCDNumber для отображения данных используется метод display()
-        self.LCD_count.display(self.count)
-
-
-class FirstForm(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        # self.initUI()
         uic.loadUi('calc.ui', self)
-        self.setFixedSize(400, 600)
-
+        self.initUI()
+        self.binding_keys()
+        self.status = 0
     def initUI(self):
-        self.setGeometry(300, 300, 300, 300)
-        self.setWindowTitle('Главная форма')
-
-        self.btn = QPushButton('Другая форма', self)
-        self.btn.resize(self.btn.sizeHint())
-        self.btn.move(100, 100)
-
-        self.btn.clicked.connect(self.open_second_form)
-
-    def open_second_form(self):
-        self.second_form = SecondForm(self, "Данные для второй формы")
-        self.second_form.show()
+        self.setFixedSize(400, 620)
+        self.setWindowTitle('Calculator')
+        self.Status.setText('10 -> 3')
 
 
-class SecondForm(QWidget):
-    def __init__(self, *args):
-        super().__init__()
-        self.initUI(args)
 
-    def initUI(self, args):
-        self.setGeometry(300, 300, 300, 300)
-        self.setWindowTitle('Вторая форма')
-        self.lbl = QLabel(args[-1], self)
-        self.lbl.adjustSize()
+    def binding_keys(self):
+        self.AuthorMenu.triggered.connect(self.show_author_info)
+        self.ClearMenu.triggered.connect(self.clear_monitor)
 
+        self.PoundButton.clicked.connect(self.button_input)
+        self.Button0.clicked.connect(self.button_input)
+        self.Button1.clicked.connect(self.button_input)
+        self.Button2.clicked.connect(self.button_input)
+        self.Button3.clicked.connect(self.button_input)
+        self.Button4.clicked.connect(self.button_input)
+        self.Button5.clicked.connect(self.button_input)
+        self.Button6.clicked.connect(self.button_input)
+        self.Button7.clicked.connect(self.button_input)
+        self.Button8.clicked.connect(self.button_input)
+        self.Button9.clicked.connect(self.button_input)
+
+        self.Mode0.clicked.connect(self.choose_mode)
+        self.Mode1.clicked.connect(self.choose_mode)
+
+        self.Convert.clicked.connect(self.convert)
+        self.ClearButton.clicked.connect(self.clear_last)
+    def button_input(self):
+        """Считывание клавиш"""
+        # self.label.setText(self.sender().text())
+        text = self.sender().text()
+        text_on_monitor = self.Monitor.text()
+        self.Monitor.setText(text_on_monitor + text)
+
+    def show_author_info(self):
+        '''Вывод информации об авторе'''
+        author_info = "Выполнил Попов Ю.А. ИУ7-22Б"
+        QMessageBox.information(self, 'Об авторе', author_info)
+
+    def clear_monitor(self):
+        self.Monitor.setText('')
+
+    def clear_last(self):
+        text = self.Monitor.text()
+        text = text[:-1]
+        self.Monitor.setText(text)
+    def choose_mode(self):
+        text = self.sender().text()
+        if text == '10 -> 3':
+            self.status = 0
+        elif text == '3 -> 10':
+            self.status = 1
+
+        if self.status == 0:
+            self.Button3.setEnabled(True)
+            self.Button4.setEnabled(True)
+            self.Button5.setEnabled(True)
+            self.Button6.setEnabled(True)
+            self.Button7.setEnabled(True)
+            self.Button8.setEnabled(True)
+            self.Button9.setEnabled(True)
+            self.Status.setText('10 -> 3')
+        elif self.status == 1:
+            self.Button3.setEnabled(False)
+            self.Button4.setEnabled(False)
+            self.Button5.setEnabled(False)
+            self.Button6.setEnabled(False)
+            self.Button7.setEnabled(False)
+            self.Button8.setEnabled(False)
+            self.Button9.setEnabled(False)
+            self.Status.setText('3 -> 10')
+
+    def convert(self):
+        number = self.Monitor.text()
+        test_result = test_input_data(number, self.status)
+        if test_result == 'letters in number':
+            self.Status2.setText('letters in number')
+            self.clear_monitor()
+        elif test_result == 'too match pounds':
+            self.Status2.setText('too match pounds')
+            self.clear_monitor()
+        elif test_result == 'incorrect digits for 3cc':
+            self.Status2.setText('incorrect digits for 3cc')
+        elif test_result == '1':
+            self.Status2.setText('')
+            if self.status == 0:
+                result = convert10to3(number, precision=9)
+                self.Monitor.setText(result)
+            elif self.status == 1:
+                result = convert3to10(number, precision=9)
+                self.Monitor.setText(result)
+            else:
+                print('Что-то пошло не так')
+        else:
+            print('INCORRECT')
+            self.Status2.setText('INCORRECT')
 
 if __name__ == '__main__':
     app = QApplication(argv)
-    ex = FirstForm()
-    ex.show()
+    calc = Calculator_Interface()
+    calc.show()
     exit(app.exec())
